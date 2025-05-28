994906e7c70cd6f032542bfb48efc9c1241c89f4
Ayrgo <cedrickarnigo1723@gmail.com>
2025-05-28 15:35:30 +0800

    nice

--- Public/module/employee_create.php
+++ Public/module/employee_create.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if (!isset($_SESSION['admin'])) {
+    header("Location: ../admin/login.php");
+    exit;
+}
+
+?>
 
 
 <!DOCTYPE html>
